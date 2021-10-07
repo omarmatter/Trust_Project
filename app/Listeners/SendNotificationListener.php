efd621169f7d6ad6f904aa321d11923f5efd40d2
omarmatter <oambd2001@gmail.com>
2021-10-07 11:05:53 +0300

    queue

--- app/Listeners/SendNotificationListener.php
+++ app/Listeners/SendNotificationListener.php
@@ -7,12 +7,14 @@ use App\Models\setting;
 use App\Models\User;
 use App\Notifications\NewUser;
 use App\Scopes\AdminTypeScope;
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Notification;
 
 class SendNotificationListener implements ShouldQueue
 {
+    use Queueable;
     /**
      * Create the event listener.
      *
@@ -36,10 +38,14 @@ class SendNotificationListener implements ShouldQueue
 
 
         if(config('notify')){
-        $user = $event->user;
+     $user= $event->user;
 
-        $users = User::get();
-        Notification::send($users, new NewUser($user));
+        $users = User::chunk(100, function($users) use ($user) {
+            Notification::send($users, new NewUser($user));
+           
+
+
+        });
         }
     }
 }
